c10a5412978660aa3d33655ecb4668f706990d2d
Louis Teboul <louis.teboul@izi-pass.com>
2014-04-17 11:46:16 +0200

    ColorControl: add brightness slider to control all 3 RGB channels at the same time

--- PimpMyZ1/src/com/androguide/honamicontrol/kernel/colorcontrol/ColorControlActivity.java
+++ PimpMyZ1/src/com/androguide/honamicontrol/kernel/colorcontrol/ColorControlActivity.java
@@ -28,6 +28,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.androguide.honamicontrol.R;
 import com.androguide.honamicontrol.helpers.CMDProcessor.CMDProcessor;
@@ -41,17 +42,20 @@ public class ColorControlActivity extends ActionBarActivity implements ColorCont
         //noinspection ConstantConditions
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setIcon(getResources().getDrawable(R.drawable.ic_tools_color_calibration));
-        setContentView(R.layout.card_gamma_control);
+        setContentView(R.layout.card_color_control);
         final SharedPreferences bootPrefs = getSharedPreferences("BOOT_PREFS", 0);
 
-        SeekBar redSeek = (SeekBar) findViewById(R.id.red_seekbar);
-        SeekBar greenSeek = (SeekBar) findViewById(R.id.green_seekbar);
+        final SeekBar brightnessSeek = (SeekBar) findViewById(R.id.brightness_seekbar);
+        final SeekBar redSeek = (SeekBar) findViewById(R.id.red_seekbar);
+        final SeekBar greenSeek = (SeekBar) findViewById(R.id.green_seekbar);
         final SeekBar blueSeek = (SeekBar) findViewById(R.id.blue_seekbar);
 
+        final TextView brightnessVal = (TextView) findViewById(R.id.brightness_unit);
         final TextView redVal = (TextView) findViewById(R.id.red_unit);
         final TextView greenVal = (TextView) findViewById(R.id.green_unit);
         final TextView blueVal = (TextView) findViewById(R.id.blue_unit);
 
+        brightnessSeek.setMax(255);
         redSeek.setMax(255);
         greenSeek.setMax(255);
         blueSeek.setMax(255);
@@ -60,6 +64,7 @@ public class ColorControlActivity extends ActionBarActivity implements ColorCont
         final int[] currRed = {255};
         final int[] currGreen = {255};
         final int[] currBlue = {255};
+        final int[] currBrightness = {255};
 
         try {
             currRed[0] = Integer.parseInt(rawCalib[0]);
@@ -69,6 +74,17 @@ public class ColorControlActivity extends ActionBarActivity implements ColorCont
             Log.e("GammaControl", e.getMessage());
         }
 
+        if (currRed[0] == currGreen[0] && currRed[0] == currBlue[0]) {
+            currBrightness[0] = currRed[0];
+            brightnessSeek.setProgress(currBrightness[0]);
+            brightnessVal.setText(getString(R.string.color_brightness) + " " + currBrightness[0]);
+        } else {
+            currBrightness[0] = 255;
+            brightnessSeek.setProgress(255);
+            brightnessSeek.setEnabled(false);
+            brightnessVal.setText(getString(R.string.custom));
+        }
+
         redSeek.setProgress(currRed[0]);
         greenSeek.setProgress(currGreen[0]);
         blueSeek.setProgress(currBlue[0]);
@@ -76,6 +92,38 @@ public class ColorControlActivity extends ActionBarActivity implements ColorCont
         redVal.setText(getString(R.string.red_val) + " " + currRed[0]);
         greenVal.setText(getString(R.string.green_val) + " " + currGreen[0]);
         blueVal.setText(getString(R.string.blue_val) + " " + currBlue[0]);
+
+        brightnessSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean fromUser) {
+                currBrightness[0] = i;
+                currRed[0] = i;
+                currGreen[0] = i;
+                currBlue[0] = i;
+                brightnessVal.setText(ColorControlActivity.this.getString(R.string.color_brightness) + " " + currBrightness[0]);
+
+                redSeek.setProgress(i);
+                blueSeek.setProgress(i);
+                greenSeek.setProgress(i);
+                redVal.setText(getString(R.string.red_val) + " " + currRed[0]);
+                greenVal.setText(getString(R.string.green_val) + " " + currGreen[0]);
+                blueVal.setText(getString(R.string.blue_val) + " " + currBlue[0]);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (currBrightness[0] < 2)
+                    Toast.makeText(ColorControlActivity.this, getString(R.string.color_brightness_warning), Toast.LENGTH_LONG).show();
+                else
+                    CMDProcessor.runSuCommand("busybox echo \'" + currRed[0] + " " + currGreen[0] + " " + currBlue[0] + "\' > " + GAMMA_KCAL + "\nbusybox echo 1 > " + GAMMA_OK);
+                    bootPrefs.edit().putString("KCAL_CONFIG", currRed[0] + " " + currGreen[0] + " " + currBlue[0]).commit();
+            }
+        });
 
         redSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -93,6 +141,17 @@ public class ColorControlActivity extends ActionBarActivity implements ColorCont
             public void onStopTrackingTouch(SeekBar seekBar) {
                 CMDProcessor.runSuCommand("busybox echo \'" + currRed[0] + " " + currGreen[0] + " " + currBlue[0] + "\' > " + GAMMA_KCAL + "\nbusybox echo 1 > " + GAMMA_OK);
                 bootPrefs.edit().putString("KCAL_CONFIG", currRed[0] + " " + currGreen[0] + " " + currBlue[0]).commit();
+
+                if (currRed[0] == currGreen[0] && currRed[0] == currBlue[0]) {
+                    currBrightness[0] = currRed[0];
+                    brightnessSeek.setProgress(currBrightness[0]);
+                    brightnessVal.setText(getString(R.string.color_brightness) + " " + currBrightness[0]);
+                } else {
+                    currBrightness[0] = 255;
+                    brightnessSeek.setProgress(255);
+                    brightnessSeek.setEnabled(false);
+                    brightnessVal.setText(getString(R.string.custom));
+                }
             }
         });
 
@@ -112,6 +171,17 @@ public class ColorControlActivity extends ActionBarActivity implements ColorCont
             public void onStopTrackingTouch(SeekBar seekBar) {
                 CMDProcessor.runSuCommand("busybox echo \'" + currRed[0] + " " + currGreen[0] + " " + currBlue[0] + "\' > " + GAMMA_KCAL + "\nbusybox echo 1 > " + GAMMA_OK);
                 bootPrefs.edit().putString("KCAL_CONFIG", currRed[0] + " " + currGreen[0] + " " + currBlue[0]).commit();
+
+                if (currRed[0] == currGreen[0] && currRed[0] == currBlue[0]) {
+                    currBrightness[0] = currRed[0];
+                    brightnessSeek.setProgress(currBrightness[0]);
+                    brightnessVal.setText(getString(R.string.color_brightness) + " " + currBrightness[0]);
+                } else {
+                    currBrightness[0] = 255;
+                    brightnessSeek.setProgress(255);
+                    brightnessSeek.setEnabled(false);
+                    brightnessVal.setText(getString(R.string.custom));
+                }
             }
         });
 
@@ -131,6 +201,17 @@ public class ColorControlActivity extends ActionBarActivity implements ColorCont
             public void onStopTrackingTouch(SeekBar seekBar) {
                 CMDProcessor.runSuCommand("busybox echo \'" + currRed[0] + " " + currGreen[0] + " " + currBlue[0] + "\' > " + GAMMA_KCAL + "\nbusybox echo 1 > " + GAMMA_OK);
                 bootPrefs.edit().putString("KCAL_CONFIG", currRed[0] + " " + currGreen[0] + " " + currBlue[0]).commit();
+
+                if (currRed[0] == currGreen[0] && currRed[0] == currBlue[0]) {
+                    currBrightness[0] = currRed[0];
+                    brightnessSeek.setProgress(currBrightness[0]);
+                    brightnessVal.setText(getString(R.string.color_brightness) + " " + currBrightness[0]);
+                } else {
+                    currBrightness[0] = 255;
+                    brightnessSeek.setProgress(255);
+                    brightnessSeek.setEnabled(false);
+                    brightnessVal.setText(getString(R.string.custom));
+                }
             }
         });
 
